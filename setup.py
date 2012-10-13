747939416818b176ad642fda4b62f01729abea05
Andreas Zeidler <az@zitc.de>
2012-10-13 16:54:18 +0000

    Prepare z3c.objpath 1.1.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = (
 
 setup(
     name='z3c.objpath',
-    version='1.1dev',
+    version='1.1',
     description="Generate and resolve paths to to objects.",
     long_description=long_description,
     classifiers=[],
