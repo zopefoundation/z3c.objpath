541f703240096e3b7360db56370d6d5f5e2279d5
Michael Howitz <icemac@gmx.net>
2026-03-17 08:56:12 +0100

    Update Python version support.

--- setup.py
+++ setup.py
@@ -21,11 +21,11 @@ setup(
     long_description=long_description,
     classifiers=[
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'License :: OSI Approved :: Zope Public License',
@@ -37,7 +37,7 @@ setup(
     license='ZPL',
     include_package_data=True,
     zip_safe=False,
-    python_requires='>=3.9',
+    python_requires='>=3.10',
     install_requires=[
         'setuptools',
         'zope.interface',
