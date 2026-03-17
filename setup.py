b4abe17b4ad4d01cde0a81424185ed89a101ca32
Michael Howitz <icemac@gmx.net>
2026-03-17 08:59:53 +0100

    Move package metadata from setup.py to pyproject.toml.

--- setup.py
+++ setup.py
@@ -1,46 +1,5 @@
-import os
-
 from setuptools import setup
 
 
-def read(*rnames):
-    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
-        return f.read()
-
-
-long_description = (
-    read('src', 'z3c', 'objpath', 'README.rst')
-    + '\n' +
-    read('CHANGES.rst')
-)
-
-setup(
-    name='z3c.objpath',
-    version='3.1.dev0',
-    description="Generate and resolve paths to to objects.",
-    long_description=long_description,
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
-        'Programming Language :: Python :: 3.14',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'License :: OSI Approved :: Zope Public License',
-    ],
-    keywords='Zope',
-    author='Martijn Faassen',
-    author_email='faassen@startifact.com',
-    url='https://github.com/zopefoundation/z3c.objpath',
-    license='ZPL',
-    include_package_data=True,
-    zip_safe=False,
-    python_requires='>=3.10',
-    install_requires=[
-        'setuptools',
-        'zope.interface',
-    ],
-    entry_points={},
-)
+# See pyproject.toml for package metadata
+setup()
